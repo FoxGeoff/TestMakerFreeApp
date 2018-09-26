e7186dfda0c2020664d9636327596bcdac8c8488
FoxGeoff <fox.geoffrey@gmail.com>
2018-09-26 14:35:46 -0700

    Add ActionResult ByTitle

--- TestMakerFreeWebApp/Controllers/QuizController.cs
+++ TestMakerFreeWebApp/Controllers/QuizController.cs
@@ -38,5 +38,23 @@ namespace TestMakerFreeWebApp.Controllers
                     Formatting = Formatting.Indented
                 });
         }
+        ///<summary>
+        ///GET: api/quiz/ByTitle
+        ///Retrieves the {num} Quizzes sorted by Title (A to Z)
+        ///</summary>
+        ///<param name="num">the number of quizzes to retrieve</param>
+        ///<returns>{num} Quizzes sorted by Title</returns>
+        [HttpGet("ByTitle/{num:int?}")]
+        public IActionResult ByTitle(int num = 10)
+        {
+            var sampleQuizzes = ((JsonResult)Latest(num)).Value as List<QuizViewModel>;
+
+            return new JsonResult(
+                sampleQuizzes.OrderBy(t => t.Title),
+                new JsonSerializerSettings()
+                {
+                    Formatting = Formatting.Indented
+                });
+        }
     }
 }

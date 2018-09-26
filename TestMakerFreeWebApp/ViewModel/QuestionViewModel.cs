31970691fa4eaa2945de6f845b1ca1702b97db9a
FoxGeoff <fox.geoffrey@gmail.com>
2018-09-26 16:09:10 -0700

    Add QuestionViewModel

---
+++ TestMakerFreeWebApp/ViewModel/QuestionViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.ComponentModel;
+
+namespace TestMakerFreeWebApp.ViewModel
+{
+    public class QuestionViewModel
+    {
+        public QuestionViewModel()
+        {
+
+        }
+
+        public int Id { get; set; }
+        public int QuizId { get; set; }
+        public string Text { get; set; }
+        public string Notes { get; set; }
+        [DefaultValue(0)]
+        public int Type { get; set; }
+        [JsonIgnore]
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastModiFiedDate { get; set; }
+    }
+}

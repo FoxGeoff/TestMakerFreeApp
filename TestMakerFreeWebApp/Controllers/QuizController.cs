ccaf632f3810e1a001259646a681700c0d1b6054
FoxGeoff <fox.geoffrey@gmail.com>
2018-09-26 13:13:29 -0700

    Add QuizConroller Latest Method

--- TestMakerFreeWebApp/Controllers/QuizController.cs
+++ TestMakerFreeWebApp/Controllers/QuizController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TestMakerFreeWebApp.ViewModel;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,36 +13,30 @@ namespace TestMakerFreeWebApp.Controllers
     [Route("api/[controller]")]
     public class QuizController : Controller
     {
-        // GET: api/<controller>
-        [HttpGet]
-        public IEnumerable<string> Get()
+        //GET api/quiz/latest
+        [HttpGet("Latest/{num}")]
+        public IActionResult Latest(int num = 10)
         {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/<controller>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<controller>
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/<controller>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/<controller>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            var sampleQuizzes = new List<QuizViewModel>();
+            //add a first sample quiz
+            for (int i = 2; i < num; i++)
+            {
+                sampleQuizzes.Add(new QuizViewModel()
+                {
+                    Id = 1,
+                    Title = @"Sample Quiz {i}",
+                    Description = "This is a quiz",
+                    CreatedDate = DateTime.Now,
+                    LastModifiedDate = DateTime.Now
+                });
+            }
+            //output the result in JSON format
+            return new JsonResult(
+                sampleQuizzes,
+                new JsonSerializerSettings()
+                {
+                    Formatting = Formatting.Indented
+                });
         }
     }
 }
